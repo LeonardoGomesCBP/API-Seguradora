336a2766898f31b14685d681d3785f5a7df94519
LeonardoGomesCBP <leonardogomescbp@gmail.com>
2022-02-07 14:06:26 -0300

    últimas mudanças

--- src/main/java/com/apiseguradora/service/ClienteAtualizarService.java
+++ src/main/java/com/apiseguradora/service/ClienteAtualizarService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.apiseguradora.exception.ApiMessage;
 import com.apiseguradora.model.Cliente;
 import com.apiseguradora.repository.ClienteRepository;
+import com.apiseguradora.utils.Utils;
 
 
 @Service
@@ -17,6 +18,9 @@ public class ClienteAtualizarService {
 	@Autowired
 	ClienteRepository clienteRepository;
 	
+	
+	Cliente cli = new Cliente();
+
 	public ResponseEntity<Object> atualizarCliente (Long id, Cliente Cliente) {
 		Optional<Cliente> clienteBanco = clienteRepository.findById(id);
 
@@ -26,8 +30,17 @@ public class ClienteAtualizarService {
 			_cliente.setCidade(Cliente.getCidade());
 			_cliente.setCpf(Cliente.getCpf());
 			_cliente.setUf(Cliente.getUf());
-
-			return new ResponseEntity<>(clienteRepository.save(_cliente), HttpStatus.OK);
+			
+			try {
+				if (Utils.isValidCPF(Cliente.getCpf().replaceAll("[^0-9]", ""))) {
+					Cliente cliente = clienteRepository.save(cli);
+					return new ResponseEntity<>(_cliente, HttpStatus.CREATED);
+				} else {
+					return new ResponseEntity<>("CPF não é válido!", HttpStatus.PRECONDITION_REQUIRED);
+				}
+			} catch (Exception e) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
 		} else {
 			return new ResponseEntity<>(new ApiMessage("Cliente não encontrado"), HttpStatus.NOT_FOUND);
 		}
